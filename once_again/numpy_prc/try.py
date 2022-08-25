6c52fb986ac490e0b7b66f3aeff7ffea00a4eca9
sanidhya <sanidhyasoni655@gmail.com>
2022-08-25 11:32:48 +0530

    Me

--- once_again/numpy_prc/try.py
+++ once_again/numpy_prc/try.py
@@ -163,8 +163,110 @@ print(ys)
 z = np.sqrt(xs ** 2 + ys ** 2)
 print(z)
 
-import matplotlib.pyplot as plt
 
-plt.imshow(z, cmap=plt.cm.gray); plt.colorbar()
-plt.title('Image plot of $\sqrt{x^2 + y^2}$ for a grid of values')
-plt.show()
+# Plot unavailable on linux
+# import matplotlib.pyplot as plt
+#
+# plt.imshow(z, cmap=plt.cm.gray); plt.colorbar()
+# plt.title('Image plot of $\sqrt{x^2 + y^2}$ for a grid of values')
+# plt.show()
+
+xarr = np.array([1.1, 1.2, 1.3, 1.4, 1.5])
+yarr = np.array([2.1, 2.2, 2.3, 2.4, 2.5])
+cond = np.array([True, False, True, True, False])
+
+# Take from xarr if True else take from yarr
+# Using list comprehension
+result = [(x if c else y)
+          for x, y, c in zip(xarr, yarr, cond)]
+print(result)
+
+# Using NumPy
+print(np.where(cond, xarr, yarr))
+
+arr = np.random.randn(4, 4)
+print(arr)
+
+print(arr > 0)
+
+print(np.where((arr > 0), 2, -2))
+print(np.where((arr > 0), 2, arr))
+
+# Mathematical and Statistical Methods
+arr = np.random.randn(5, 4)
+print(arr)
+
+print(arr.mean())
+print(np.mean(arr))
+print(arr.sum() / arr.size)
+
+print(arr.mean(axis=1))
+print(arr.mean(axis=0))
+print(arr.sum(axis=0))
+print(arr.sum(axis=1))
+
+arr = np.array([0, 1, 2, 3, 4, 5, 6, 7])
+print(arr.cumsum())
+
+arr = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+print(arr.cumsum(axis=0))
+print(arr.cumprod(axis=1))
+
+# Methods for Boolean Array
+arr = np.random.randn(100)
+print(arr)
+print((arr > 0).sum())  # This will give us the total number of positive values
+
+bools = np.array([False, False, True, False])
+print(bools.any())  # Checks if one or more values are true
+print(bools.all())  # Checks if all the values are true
+# Above method can work with non booleans also considering non-zero values as true and zeros as false
+
+# Sorting
+arr = np.random.randn(6)
+print(arr)
+
+arr.sort()
+print(arr)
+
+# Sorting in Multidimensional Arrays
+arr = np.random.randn(5, 3)
+print(arr)
+arr.sort(1)
+print(arr)
+arr.sort(0)
+print(arr)
+
+large_arr = np.random.randn(1000)
+print(large_arr[int(0.05 * len(large_arr))])  # 5% quantile
+
+# Unique and Other Set Logic
+names = np.array(['Bob', 'Joe', 'Will', 'Bob', 'Will', 'Joe', 'Joe'])
+print(np.unique(names))  # Will give unique elements in sorted order
+print(sorted(set(names)))  # Alternative in Python
+
+ints = np.array([3, 3, 3, 2, 2, 1, 1, 4, 4])
+print(np.unique(ints))
+
+values = np.array([6, 0, 0, 3, 2, 5, 6])
+print(np.in1d(values, [2, 3, 6]))  # This function checks if the values of 1 array exist in second or not
+
+x = np.array([[1., 2., 3.], [4., 5., 6.]])
+y = np.array([[6., 23.], [-1, 7], [8, 9]])
+print(x.dot(y))
+print(np.dot(x, y))
+
+print(np.dot(x, np.ones(3)))
+print(x @ np.ones(3))
+
+'''from numpy.linalg import inv, qr  # Library for operations like matrix decomposition or inverse and determinant
+
+X = np.random.randn(5, 5)
+mat = X.T.dot(X)  # Dot product with transpose of matrix
+print(mat)
+inv(mat)  # Compute the inverse of a matrix
+mat = mat.dot(inv(mat))
+print(mat)'''
+
+samples = np.random.normal(size=(4, 4))  # Faster than normal random function of python
+
